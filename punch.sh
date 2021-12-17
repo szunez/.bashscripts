d4c1e70b922098963ebc0bb185c80e90129bd9a9
Stephanos Zunez <stephanos.zunez@evoleap.com>
2021-12-16 18:22:19 -0600

    chronological time log with format utcTime,yyyy.mm.dd-hh:mm

---
+++ punch.sh
@@ -0,0 +1,59 @@
+function punch() {
+    utcTime=`date +%s`
+    (( SECONDS = $utcTime ))
+    echo 'report @UTC='$SECONDS' | '`date +%H:%M:%S`
+    if [ "$1" == "--in" ] || [ "$1" == "-i" ]; then
+        echo $SECONDS','`date +%Y.%m.%d-%H:%M`',in,'$2 >> ~/time.log
+    elif [ "$1" == "--out" ] || [ "$1" == "-o" ]; then
+        lastTime=`echo $( tail -n 1 ~/time.log ) | cut -f1 -d','`
+        delaTime=`printf %.2f "$((10**3 * ($SECONDS-$lastTime)/3600))e-3"`
+        echo $SECONDS','`date +%Y.%m.%d-%H:%M`',out,'$2','$delaTime' hours,'>> ~/time.log
+    elif [ "$1" == "--summary" ] || [ "$1" == "-s" ]; then
+        lines=`wc -l ~/time.log | cut -f1 -d' '`
+        if [ "$2" == "" ]; then
+            (( days = 43200 ))
+        else
+            (( days = $2*86400 ))
+        fi
+        curTime=$SECONDS
+        (( startTime = $curTime - $days ))
+        lastTime=`echo $( tail -n 1 ~/time.log ) | cut -f1 -d','`
+        task=()
+        dur=()
+        (( i = 1 ))
+        until [ "$startTime" -gt "$lastTime" ]; do
+            if [ "$i" -gt "$lines" ]; then
+                break
+            fi
+            if [ "`echo $( tail -n $i ~/time.log ) | cut -f3 -d','`" == "out" ]; then
+                task[$i-1]=`echo $( tail -n $i ~/time.log ) | cut -f4 -d','`
+                dur[$i-1]=`echo $( tail -n $i ~/time.log ) | cut -f5 -d',' | cut -f1 -d' '`
+            fi
+            (( i++ ))
+            lastTime=`echo $( tail -n $i ~/time.log ) | cut -f1 -d','`
+        done
+        taskSummary=(`echo ${task[@]} | xargs -n1 | sort -u | xargs`)
+        durSummary=()
+        durTotal=""
+        (( taskLimit = ${#task[@]}*2 ))
+        echo ''
+        echo '──────────┬──────────────────────'
+        echo ' duration │   task'
+        echo '[ hours ] │ description'
+        echo '──────────┼──────────────────────'
+        for (( k=0; k<${#taskSummary[@]}; k++ )) do
+            for (( j=0; j<=$taskLimit; j++ )) do 
+                if [ "${task[$j]}" == "${taskSummary[$k]}" ]; then
+                    durSummary[$k]=$(awk '{print $1+$2}' <<<"${dur[$j]} ${durSummary[$k]}")
+                fi
+            done
+        durTotal=$(awk '{print $1+$2}' <<<"$durTotal ${durSummary[$k]}")
+        padding0='       '
+        printf "%s%s │ %s\n" '  '${durSummary[$k]} "${padding0:${#durSummary[$k]}}" '  '${taskSummary[$k]}
+        done
+        echo '══════════╦══════════════════════'
+        printf "%s%s ║ %s\n" '  '$durTotal "${padding0:${#durTotal}}" '  '"period total"
+    elif [ "$1" == "--edit" ] || [ "$1" == "-e" ]; then
+        getTimelog
+    fi
+}
