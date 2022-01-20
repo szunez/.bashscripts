06a312dd995961bf525c713d3f91c0e9251dba1f
Stephanos Zunez <stephanos.zunez@gmail.com>
2022-01-20 10:24:14 -0600

    bug fix for edit mode

--- watchlist.sh
+++ watchlist.sh
@@ -1,6 +1,7 @@
 function watchlist () {
     if [ "$1" == "--edit" ] || [ "$1" == "-e" ]; then
         code "$funnelDir"/watchlist.csv
+    else
+        sed -i "1s/^/`date +%Y-%m-%dT%H:%M`,$1,$2,$3\n/" "$funnelDir"/watchlist.csv
     fi
-    sed -i "1s/^/`date +%Y-%m-%dT%H:%M`,$1,$2,$3\n/" "$funnelDir"/watchlist.csv
 }
