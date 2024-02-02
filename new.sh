3c3569b76170815e33efcad71ffb5cb4bddc52d6
Stephanos Zunez <stephanos.zunez@gmail.com>
2024-02-02 09:33:28 -0600

    added new meeting type

--- new.sh
+++ new.sh
@@ -14,6 +14,12 @@ function new() {
         getNotesTemplate $2 ./`date +%Y%m%d`'-'$2'-notes.txt'
         code ./`date +%Y%m%d`'-'$2'-notes.txt'
         explorer .;
+    elif [ "$1" == "--elm-meeting" ]; then
+        cd "$funnelDir"/meetings;
+        > `date +%Y%m%d`'-'$2'-elm-notes.txt';
+        getElmNotesTemplate $2 ./`date +%Y%m%d`'-'$2'-elm-notes.txt'
+        code ./`date +%Y%m%d`'-'$2'-elm-notes.txt'
+        explorer .;
     elif [ "$1" == "--sale" ]; then
         gosales
         nproject=`getNumber`
@@ -48,4 +54,7 @@ function new() {
 }
 function getNotesTemplate() {
     printf "%s %s\n%s\n        \n%s\n        \n%s\n        \n%s\n        " `date +%Y%m%d` $1 "    attendees" "    topics" "    questions" "    actionItems" >> $2
+}
+function getElmNotesTemplate() {
+    printf "%s %s\n%s\n        \n%s        \n%s            \n%s            \n%s            \n%s\n            \n%s            \n%s\n            \n%s\n            \n%s\n            \n%s\n        \n%s        \n%s        \n%s        \n%s\n    \n%s\n        \n%s        \n%s        \n%s        \n%s        \n%s        \n%s        \n%s\n        \n%s        \n    \n%s\n        " `date +%Y%m%d` $1 "    attendees" "      topics" "        Intro" "            [ufa] I understood that you have 30min. We want to respect that, so we will wrap up right at 30min unless you want to go over time more." "            We wanted to understand your objectives in this meeting.  Could you please give us a quick high level summary of your main goal today?" "            Our objective is to introduce the elm technology and learn if there may be a match with your project. If there is still interest at the end of the meeting we would like to schedule a demo or connect you with one of our senior developers to answer questions in-depth." "            [s] Can you share some details about the project you were interested in using elm on?" "            Where does this project sit among your priorities?" "            [p] Can you describe some challenges/problems that you have been facing?" "            [i] Can you share what the implications of the challenges you described are for your company and this project?" "            [n] What are some of the expected benefits that you would hope to get from elm?" "        Access management:" "        Account management:" "        Product management:" "        Best practises / scale-up:" "    questions" "        Company:" "        Company age/history:" "        Other solutions in consideration:" "        Role:" "        Products:" "        Industry:" "        Customer base:" "        Problems        |       Trigger" "    actionItems" >> $2
 }
